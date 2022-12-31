d9848143763304d387aa35339414736ec8c3a846
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-31 05:22:59 +0200

    done

---
+++ main.mjs
@@ -0,0 +1,6 @@
+// made the request with this command:
+// $ curl  https://jsonplaceholder.typicode.com/posts/1/comments --verbose --output res.json
+
+// the Content-Type is application/json
+
+// the response body exists in ./res.json
